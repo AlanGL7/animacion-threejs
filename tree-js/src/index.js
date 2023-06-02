fcabb11902371d871b5becdb6e7c9d1eb1929a87
AlanGL7 <37215757+AlanGL7@users.noreply.github.com>
2023-06-02 08:42:27 -0600

    Actualización de diseño

---
+++ tree-js/src/index.js
@@ -0,0 +1,57 @@
+
+alert("Usa las teclas W A S D para rotar el cubo y la barra espaciadora para restablecer la posición");
+
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
+const material = new THREE.MeshBasicMaterial( { color: 0xfedcba } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	//cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+animate();
+
+document.onkeydown = function (e) {
+    console.log(e);
+    if (e.keyCode == 87) {
+    cube.rotation.x -= 0.10;
+    }
+    if (e.keyCode == 65) {
+    cube.rotation.y -= 0.10;
+    }
+    if (e.keyCode == 83) {
+    cube.rotation.x += 0.10;
+    }
+    if (e.keyCode == 68) {
+    cube.rotation.y += 0.10;
+    }
+    if (e.keyCode == 32) {
+        if(cube.rotation.x != 0){
+            cube.rotation.x = 0;
+        }else{
+            
+        }
+        if(cube.rotation.y != 0){
+            cube.rotation.y = 0;
+        }else{
+            
+        }
+    }
+
+}
